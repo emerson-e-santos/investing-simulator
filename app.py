343d9bc327703791a6cda83f77766e1278d55e94
Emerson Santos <santos.emerson@unifesp.br>
2026-01-15 22:21:16 -0300

    Update inserido código para plot de evolução mensal do patrimônio

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from calculations import calcular_aporte_mensal
+from calculations import (calcular_aporte_mensal, evolucao_patrimonio)
 from scenarios import PERFIS
 
 st.set_page_config(page_title="Simulador de Patrimônio", layout="wide")
@@ -35,8 +35,27 @@ with st.sidebar:
 taxa = PERFIS[perfil]
 aporte = calcular_aporte_mensal(patrimonio, prazo, taxa, capital)
 
+df_evolucao = evolucao_patrimonio(
+    patrimonio,
+    prazo,
+    taxa,
+    capital
+)
+
 st.subheader("Aporte mensal necessário")
 st.metric("Valor mensal", f"R$ {aporte:,.2f}")
+
+st.subheader("Evolução do patrimônio ao longo do tempo")
+
+st.line_chart(
+    data=df_evolucao,
+    x="Mês",
+    y="Patrimônio"
+)
+st.caption(
+    "Nos primeiros anos, o crescimento é mais lento. "
+    "Com o tempo, os juros compostos passam a ter um impacto cada vez maior."
+)
 
 st.caption(
     "Esta ferramenta tem caráter exclusivamente educacional. Não constitui recomendação de investimento. Rentabilidade passada não garante resultados futuros."

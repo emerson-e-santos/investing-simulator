ab9dc4292077fa0b61ee142d7cf69dd35d5848d9
Emerson Santos <santos.emerson@unifesp.br>
2026-01-15 22:15:21 -0300

    Update inserido função de evolução mensal do patrimônio

--- calculations.py
+++ calculations.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def calcular_aporte_mensal(fv, anos, taxa_anual, capital_inicial=0):
     n = anos * 12
     i = taxa_anual / 12
@@ -7,3 +8,25 @@ def calcular_aporte_mensal(fv, anos, taxa_anual, capital_inicial=0):
 
     pmt = (fv - capital_inicial * (1 + i)**n) * i / ((1 + i)**n - 1)
     return round(pmt, 2)
+
+def evolucao_patrimonio(
+    fv, anos, taxa_anual, capital_inicial=0
+):
+    n = anos * 12
+    i = taxa_anual / 12
+
+    patrimonio = capital_inicial
+    aporte = calcular_aporte_mensal(
+        fv, anos, taxa_anual, capital_inicial
+    )
+
+    dados = []
+
+    for mes in range(1, n + 1):
+        patrimonio = patrimonio * (1 + i) + aporte
+        dados.append({
+            "Mês": mes,
+            "Patrimônio": patrimonio
+        })
+
+    return pd.DataFrame(dados)

a60e00a4ecf5af6de23d9b045a94a3dcebad0827
Emerson Santos <santos.emerson@unifesp.br>
2026-03-04 22:36:43 -0300

    Update correção inserido capital inicial no subheader

--- app.py
+++ app.py
@@ -46,8 +46,7 @@ with st.sidebar:
         )
     )
 st.subheader(
-    f"Objetivo: {formatar_moeda(patrimonio)} em {prazo} anos, "
-    f"partindo de {formatar_moeda(capital)}"
+    f"Objetivo: {formatar_moeda(patrimonio)} em {prazo} anos, partindo de {formatar_moeda(capital)}"
 )
 
 taxa = PERFIS[perfil]

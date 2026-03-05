71e205add065b4e71aa0c4c1b3c31bc0885ae723
Emerson Santos <santos.emerson@unifesp.br>
2026-03-04 21:19:10 -0300

    Update inserido período selecionado no sub-título

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ st.set_page_config(page_title="Simulador de Patrimônio", layout="wide")
 
 st.title("Simulador de Patrimônio & Investimentos")
 
+st.subheader(
+    f"Objetivo: R$ {patrimonio:,.0f} em {prazo} anos"
+)
+
 st.write(
     "Informe seu objetivo financeiro e veja quanto precisará investir por mês "
     "de acordo com diferentes perfis de risco."
